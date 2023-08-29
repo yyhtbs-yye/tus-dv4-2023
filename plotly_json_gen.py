413b71da11401b3d5254e4ed73304a9e71451ba6
yyhtbs-yye <yyhtbs0@gmail.com>
2023-08-29 15:56:19 +0100

    www

--- plotly_json_gen.py
+++ plotly_json_gen.py
@@ -2,9 +2,9 @@ import plotly.express as px
 import plotly.io as pio
 
 out_folder_path = "plotly_charts_json/"
-out_file_name = "line_chart_simple_400.json"
+out_file_name = "line_chart_simple_400_400.json"
 
-fig = px.line(x=["a","b","c"], y=[1,3,2], title="y=f(x)", width=400, height=400)
+fig = px.line(x=[1, 2, 3], y=[2, 4, 6], title="y=x", width=400, height=400)
 
 graphJSON = pio.to_json(fig)
 
