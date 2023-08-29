db355e4f777505c8823d1e76e5f5b69543aa898f
yyhtbs-yye <yyhtbs0@gmail.com>
2023-08-29 16:30:19 +0100

    ssdsd

---
+++ scatter_3d_chart.py
@@ -0,0 +1,38 @@
+import plotly.express as px
+import pandas as pd
+import plotly.io as pio
+
+dtypes={'Grams': float, 'Calories': float, 'Protein': float, 'Fat': float,'Sat.Fat': float, 'Fiber': float, 'Carbs': float}
+
+df = pd.read_csv("kaggle_niharika41298_nutritional_facts_for_most_common_foods.csv")
+
+def str_to_num(input):
+    
+    if not isinstance(input, str):
+        return input
+    
+    input_clean = input.replace(',', '').replace('t', '').replace('a', '').replace("'", "")
+    
+    if len(input_clean) == 0:
+        return None
+    
+    return eval(input_clean)
+
+
+for attr in dtypes:
+    df[attr] = df[attr].apply(str_to_num)
+
+df
+
+fig = px.scatter_3d(df, x="Carbs", y="Protein", z="Calories", color="Category",
+                               width=800, height=400)
+fig.update_traces(marker=dict(size=3))
+fig.show()
+
+graphJSON = pio.to_json(fig)
+
+out_folder_path = "plotly_charts_json/"
+out_file_name = "scatter_3d_chart_food_cal.json"
+
+with open(out_folder_path+out_file_name, "w") as fp:
+    fp.write(graphJSON)
